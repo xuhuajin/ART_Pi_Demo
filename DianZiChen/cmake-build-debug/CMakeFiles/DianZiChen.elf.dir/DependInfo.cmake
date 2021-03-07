4c7d48f073ca724dc6274b4579224ac89d61732d
Xuhuajin <1361783546@qq.com>
2021-03-07 19:01:49 +0800

    发布

---
+++ DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Startup/startup_stm32h750xbhx.s" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Startup/startup_stm32h750xbhx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32H750xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/bsp_delay.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/bsp_delay.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/bsp_dubug.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/bsp_dubug.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/bsp_hx711.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/bsp_hx711.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/bsp_led.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/bsp_led.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/main.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/main.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/stm32h7xx_hal_msp.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/stm32h7xx_it.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/stm32h7xx_it.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/syscalls.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/syscalls.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/sysmem.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/sysmem.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Core/Src/system_stm32h7xx.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Core/Src/system_stm32h7xx.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj"
+  "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c" "C:/Users/Xuhuajin/Desktop/Clion_stm32/DianZiChen/cmake-build-debug/CMakeFiles/DianZiChen.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32H750xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
